f686b2b11902b100270f40cdddc6b42f98d5c971
csjcode <christophersstjohn@gmail.com>
2016-05-30 18:59:27 +0700

    Basic React component  with Meteor server & Meteor.startup (2.10)

---
+++ client/main.js
@@ -0,0 +1,21 @@
+// Any JS here is automatically run
+
+// Import React library
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create React component
+
+const App = () => {
+   return (
+      <div>
+         React App #2
+      </div>
+   )
+}
+
+// Render the component to the screen - Meteor.startup to make sure DOM is fully loaded before React render.
+Meteor.startup(() => {
+   ReactDOM.render(<App />, document.querySelector('.container'));
+});

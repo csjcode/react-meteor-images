141c64ea45354c6cae783a0812f5db4ff0066293
csjcode <christophersstjohn@gmail.com>
2016-05-31 17:14:17 +0700

    Added sample axiso (AJAX) request

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
 
 // Create React component
@@ -19,4 +20,6 @@ const App = () => {
 // Render the component to the screen - Meteor.startup to make sure DOM is fully loaded before React render.
 Meteor.startup(() => {
    ReactDOM.render(<App />, document.querySelector('.container'));
+   axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then(response => console.log(response));
 });

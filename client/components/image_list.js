df9134a96abf324645c25d969d64bb0ca6ff79ac
csjcode <christophersstjohn@gmail.com>
2016-05-31 14:24:40 +0700

    Slight change to mockup

---
+++ client/components/image_list.js
@@ -0,0 +1,17 @@
+// Create image list component
+// 3 necessary steps (improt, create, export))
+// 1. Import react
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 2. Create Component
+
+const imageList = () => {
+   return (
+   <ul>
+   </ul>
+   )
+}
+
+// 3. Export Component
--- client/main.js
+++ client/main.js
@@ -4,7 +4,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 // Create React component
 
 const App = () => {

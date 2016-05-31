ce41b2c5c3a72f6feb17c4b6fe3ec7f730d851ae
csjcode <christophersstjohn@gmail.com>
2016-05-31 18:01:29 +0700

    Changed App to class Component

--- client/main.js
+++ client/main.js
@@ -2,19 +2,20 @@
 
 // Import React library
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
 // Create React component
-
-const App = () => {
-   return (
-      <div>
-         <ImageList />
-      </div>
-   )
+class App extends Component {
+   render() {
+      return (
+         <div>
+            <ImageList />
+         </div>
+      )
+   }
 }
 
 // Render the component to the screen - Meteor.startup to make sure DOM is fully loaded before React render.

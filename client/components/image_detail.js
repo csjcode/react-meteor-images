bbc858a7a13d454ab427b7c045fe93b73bae584c
csjcode <christophersstjohn@gmail.com>
2016-06-02 14:42:00 +0700

    Revert "Added description to ImageDetail"
    This reverts commit 681d576f01d4ba080792ebdf97747084ed2f55f4.

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -14,7 +14,6 @@ const ImageDetail = (props) => {
         <h4 className="media-heading">
           {props.image.title}
         </h4>
-        <p>{props.image.description}</p>
       </div>
     </li>
   );

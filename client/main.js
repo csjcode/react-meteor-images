7367e5f559cf1d56a5e1e8b1cf799b7cf469608a
csjcode <christophersstjohn@gmail.com>
2016-06-02 11:00:14 +0700

    Setting state for Component to receive Data (2.24)

--- client/main.js
+++ client/main.js
@@ -9,7 +9,16 @@ import ImageList from './components/image_list';
 
 // Create React component
 class App extends Component {
-   render() {
+   constructor(props){
+      super(props);
+
+      this.state = { images: [] };
+   }
+   componentWillMount() {
+      axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+         .then(response => console.log(response));
+   }
+   render()  {
       return (
          <div>
             <ImageList />
@@ -21,6 +30,5 @@ class App extends Component {
 // Render the component to the screen - Meteor.startup to make sure DOM is fully loaded before React render.
 Meteor.startup(() => {
    ReactDOM.render(<App />, document.querySelector('.container'));
-   axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-      .then(response => console.log(response));
+
 });
